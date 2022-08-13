62a46fc621fbd7ece30388f682612d2d1c644a33
Alex <alexj.ash@gmail.com>
2022-08-13 18:23:44 +0100

    completed (all tests passed) ex 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var repeatedString = "";
+    if (num < 0) {return 'ERROR'}
+    else {
+    while (num > 0) {
+      repeatedString += string;
+      num--;
+    }
+    return repeatedString;
+    }
 };
-
+console.log(repeatString(test,4));
 // Do not edit below this line
 module.exports = repeatString;
