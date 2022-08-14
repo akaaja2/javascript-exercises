ea20c728ab68563b17c2475ba6ccf52c3e8e01df
Alex <alexj.ash@gmail.com>
2022-08-14 12:27:14 +0100

    solved ex 5 but issue with jest tests running

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+//issue with jest test running
 
+const sumAll = function(a,b) {
+
+if (b<a) {
+    [a, b] = [b, a];
+    return .5 * b * (b + 1) - .5 * a * (a - 1)
+} else {
+
+return .5 * b * (b + 1) - .5 * a * (a - 1)
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll();
